f34570658cc408703c469b845e282f23a69adf65
ena02 <erkembekov.nurzhan@mail.ru>
2022-04-19 10:33:46 +0600

    Initial commit

---
+++ src/main/java/com/ena/myinsta/MyInstaApplication.java
@@ -0,0 +1,13 @@
+package com.ena.myinsta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInstaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyInstaApplication.class, args);
+    }
+
+}
