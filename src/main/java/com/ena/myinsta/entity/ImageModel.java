c47397393d05d60c82a6a9b87e81b5659e82a60a
ena02 <erkembekov.nurzhan@mail.ru>
2022-04-19 10:54:53 +0600

    I hava implemented ImageModel

--- src/main/java/com/ena/myinsta/entity/ImageModel.java
+++ src/main/java/com/ena/myinsta/entity/ImageModel.java
@@ -1,4 +1,10 @@
 package com.ena.myinsta.entity;
 
 public class ImageModel {
+
+    private Long id;
+    private String name;
+    private byte[] ImageBytes;
+    private Long userId;
+    private Long postId;
 }
