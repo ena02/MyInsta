0c5c093524d0349cfabcc5dcb8165ed49a262944
ena02 <erkembekov.nurzhan@mail.ru>
2022-04-19 10:47:17 +0600

    I hava implemented Post model

--- src/main/java/com/ena/myinsta/entity/Post.java
+++ src/main/java/com/ena/myinsta/entity/Post.java
@@ -1,4 +1,27 @@
 package com.ena.myinsta.entity;
 
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Post {
+
+    private String id;
+    private String title;
+    private String caption;
+    private String location;
+    private Integer likes;
+
+    private Set<String> likedUsers = new HashSet<>();
+    private User user;
+    private List<Comment> comments = new ArrayList<>();
+    private LocalDateTime localDateTime;
+
+    @PrePersist
+    protected void onCreate() {
+        this.localDateTime = LocalDateTime.now();
+    }
 }
