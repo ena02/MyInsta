fd59804f1841c7e194e98a96fd7035bd1b250315
ena02 <erkembekov.nurzhan@mail.ru>
2022-04-19 10:42:57 +0600

    I hava implemented User model

--- src/main/java/com/ena/myinsta/entity/User.java
+++ src/main/java/com/ena/myinsta/entity/User.java
@@ -1,4 +1,30 @@
 package com.ena.myinsta.entity;
 
+import com.ena.myinsta.entity.enums.ERole;
+import jdk.dynalink.linker.LinkerServices;
+
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class User {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String lastname;
+    private String bio;
+    private String password;
+
+    private Set<ERole> role = new HashSet<>();
+    private List<Post> posts = new ArrayList<>();
+    private LocalDateTime localDateTime;
+
+    @PrePersist
+    protected void onCreate() {
+        this.localDateTime = LocalDateTime.now();
+    }
 }
