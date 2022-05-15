b4e2142d0f8ea8037a97ae73a9ff0fd04b8ddf02
ena <erkembekov.nurzhan@mail.ru>
2022-05-15 13:56:58 +0600

    I've added annotations for user post

--- src/main/java/com/ena/myinsta/entity/Post.java
+++ src/main/java/com/ena/myinsta/entity/Post.java
@@ -1,27 +1,41 @@
 package com.ena.myinsta.entity;
 
-import javax.persistence.PrePersist;
+import lombok.Data;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Data
+@Entity
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String title;
     private String caption;
     private String location;
     private Integer likes;
 
+    @Column
+    @ElementCollection(targetClass = String.class)
     private Set<String> likedUsers = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+
+    @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER, mappedBy = "post", orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
-    private LocalDateTime localDateTime;
+
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
 
     @PrePersist
     protected void onCreate() {
-        this.localDateTime = LocalDateTime.now();
+        this.createdDate = LocalDateTime.now();
     }
 }
