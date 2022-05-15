ee7135ae3340159abdab6b567648b63898e812b3
ena <erkembekov.nurzhan@mail.ru>
2022-05-15 13:49:26 +0600

    I've added annotations for user entity

--- src/main/java/com/ena/myinsta/entity/User.java
+++ src/main/java/com/ena/myinsta/entity/User.java
@@ -1,27 +1,54 @@
 package com.ena.myinsta.entity;
 
 import com.ena.myinsta.entity.enums.ERole;
-import jdk.dynalink.linker.LinkerServices;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
 
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+@Data
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(unique = true, updatable = true)
     private String username;
+
+    @Column(nullable = false)
     private String lastname;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(columnDefinition = "text")
     private String bio;
+
+    @Column(length = 3000)
     private String password;
 
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role",
+    joinColumns = @JoinColumn("user_id"))
     private Set<ERole> role = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime localDateTime;
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
 
     @PrePersist
     protected void onCreate() {
