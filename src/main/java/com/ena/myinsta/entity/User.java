7e49405185f7f063faf5539696f7b40e5d5fab73
ena <erkembekov.nurzhan@mail.ru>
2022-05-16 14:09:46 +0600

    I've implemented user details in user and made CustomUserDetailsService

--- src/main/java/com/ena/myinsta/entity/User.java
+++ src/main/java/com/ena/myinsta/entity/User.java
@@ -4,6 +4,7 @@ import com.ena.myinsta.entity.enums.ERole;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -13,7 +14,7 @@ import java.util.*;
 @Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,8 +33,7 @@ public class User {
     private String password;
 
     @ElementCollection(targetClass = ERole.class)
-    @CollectionTable(name = "user_role",
-            joinColumns = @JoinColumn(name = "user_id"))
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     private Set<ERole> roles = new HashSet<>();
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
@@ -46,19 +46,55 @@ public class User {
     @Transient
     private Collection<? extends GrantedAuthority> authorities;
 
+    public User() {
+    }
 
+    public User(Long id,
+                String username,
+                String email,
+                String password,
+                Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
 
     @PrePersist
     protected void onCreate() {
         this.createdDate = LocalDateTime.now();
     }
 
+
     /**
      * SECURITY
      */
 
 
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
 
---
+++ src/main/java/com/ena/myinsta/services/CustomUserDetailsService.java
@@ -0,0 +1,51 @@
+package com.ena.myinsta.services;
+
+import com.ena.myinsta.entity.User;
+import com.ena.myinsta.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUsersByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username now found with username: " + username));
+
+        return build(user);
+    }
+
+    public User loadUserById(Long id) {
+        return userRepository.findUsersById(id).orElse(null);
+    }
+
+    public static User build(User user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+
+        return new User(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getAuthorities()
+        );
+    }
+}
